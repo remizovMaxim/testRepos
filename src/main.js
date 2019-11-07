369fa9f77c169fd16bb59e5634b5fd39e3cd4cba
Maxim <sharkercool96@gmail.com>
2019-11-07 17:16:12 +0300

    my commit test file

--- src/main.js
+++ src/main.js
@@ -3,3 +3,5 @@ var test = [], aaaaa = 10, bbbbbb = 11, ccccc = 12;
 
  console.log('my text: ' + test[0] + 'my text: ' + test[1] + 'my text: ' + test[2]);
  xxx = aaaaa + bbbbbb + ccccc; console.log(xxx)
+
+ var str = "my super string";
