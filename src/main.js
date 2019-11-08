0e263505db07358186db5a384e0874d67d71794e
Maxim <sharkercool96@gmail.com>
2019-11-08 12:44:21 +0300

    my 5 test prettier+eslint commit 6

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 var test = [],
-  aaaaa = 10,
-  bbbbbb = 11,
-  ccccc = 12
-test.push('gig1')
-test.push('gig2')
-test.push('gig3')
+    aaaaa = 10,
+    bbbbbb = 11,
+    ccccc = 12;
+test.push('gig1');
+test.push('gig2');
+test.push('gig3');
 
 console.log(
-  'my text: ' + test[0] + 'my text: ' + test[1] + 'my text: ' + test[2]
-)
-xxx = aaaaa + bbbbbb + ccccc
-console.log(xxx)
+    'my text: ' +
+        test[0] +
+        'my text: ' +
+        test[1] +
+        'my text: ' +
+        test[2]
+);
+xxx = aaaaa + bbbbbb + ccccc;
+console.log(xxx);
 
-var str = 'my super string'
-document.append(str)
+var str = 'my super string';
+document.append(str);
